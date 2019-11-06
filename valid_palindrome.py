8440e8d2619b359085a768a6c4e7c6343f476a3e
Chres <clc8680@rit.edu>
2019-11-06 00:50:01 -0500

    completed valid palindrome problem

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,8 +1,53 @@
+"""
+Figure out if a string is a valid palindrome
+
+"""
+
 
 def valid_palindrome(string):
+    """
+    Returns whether or not a string is a valid palindrome
+    :param string: input string
+    :return: Boolean
+    """
+
+    if len(string) is 1 or len(string) is 0:
+        return True
+
+    middle_index = -1
+    if len(string) % 2:
+        middle_index = len(string)/2
+
     for i in range(0, len(string)):
-        pass
+        if middle_index == i:
+            return True
+        elif string[i] is not string[len(string) - 1 - i]:
+            return False
+    return True
 
 
 if __name__ == '__main__':
-    pass
+    # Test Case 1: odd length palindrome
+    # Expected: True
+    print(valid_palindrome("racecar"))
+
+    # Test Case 2: even length palindrome
+    # Expected: True
+    print(valid_palindrome("aabbccbbaa"))
+
+    # Test Case 3: not a palindrome
+    # Expected: False
+    print(valid_palindrome("racerace"))
+
+    # Test Case 4: short palindrome
+    # Expected: True
+    print(valid_palindrome("aba"))
+
+    # Test Case 5: palindrome with one letter
+    # Expected: True
+    print(valid_palindrome("a"))
+
+    # Test Case 6: palindrome with no letters
+    # Expected: True
+    print(valid_palindrome(""))
+

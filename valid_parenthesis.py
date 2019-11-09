7d2e1ea20819e76e31c87ad33c9893b0da7d3d8e
Chres <clc8680@rit.edu>
2019-11-09 00:17:41 -0500

    testing for valid parenthesis

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,21 +1,35 @@
 """
+Checks to see if a string of parenthesis is valid
+Valid - "()()()"
+Valid - "{{}}"
+Valid - "([]){}"
+Invalid - "(()"
+Invalid - "([)]"
 
 """
 
 
 def valid_parenthesis(string):
+    """
+    Checks to see if a string of parenthesis is valid
+    Time complexity: O(n)
+    Space complexity: O(n) n/2 values could be stored
+    :param string:
+    :return:
+    """
+    if len(string) % 2:
+        print("Invalid Parenthesis")
+        return
     stack = []
     bracket_hash = {"}": "{", ")": "(", "]": "["}
 
-    valid = True
-
     for bracket in string:
         if bracket in bracket_hash:
-            stack.append(bracket)
             if stack:
                 top = stack.pop()
                 if bracket_hash[bracket] is not top:
-                    valid = False
+                    print("Invalid Parenthesis")
+                    return
             else:
                 top = ""
         else:
@@ -23,7 +37,37 @@ def valid_parenthesis(string):
                 stack.append(bracket)
             else:
                 print("Invalid String")
+                return
 
-    if valid:
+    if not stack:
         print("Valid Parenthesis")
+        return
+
+
+if __name__ == '__main__':
+
+    # Test 1: "{{{}}}"
+    print("Test 1: {{{}}}")
+    valid_parenthesis("{{{}}}")
+    print()
+
+    # Test 2: "{}[]()"
+    print("Test 2: {}[]()")
+    valid_parenthesis("{}[]()")
+    print()
+
+    # Test 3: "[{}]()"
+    print("Test 3: [{}]()")
+    valid_parenthesis("[{}]()")
+    print()
+
+    # Test 4: "{{]"
+    print("Test 4: {{]")
+    valid_parenthesis("[{}]()")
+    print()
+
+    # Test 5: "[){})({"
+    print("Test 5: [){})({")
+    valid_parenthesis("[){})({")
+    print()
 

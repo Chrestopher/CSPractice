e4795bc7fb896a7a70602d9aaecd66df714dd8e2
Chres <clc8680@rit.edu>
2019-11-12 23:20:44 -0500

    added recursive reversal with testing, comments, and analysis

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -3,6 +3,10 @@ Reversing a linked list
 
 Tips: When you are reversing a linked list, there are three parts you need to keep track while changing the pointers:
 The previous, the current, and the next.
+
+For recursion, try to traverse to the last node and see how you can manipulate the current node's pointer to point to the correct node.
+As recursion goes, it will return the value of the last node backwards into the previous node's stack trace. As a result, you can assign pointers
+in the opposite direction.
 """
 
 
@@ -21,10 +25,29 @@ class LinkedNode:
 
 
 def reverse_linked_list_recursive(node):
-    pass
+    """
+    Reverses a Linked List with recursion
+    Time complexity: O(n)
+    Space complexity: O(n)
+    :param node:
+    :return:
+    """
+    if node is None or node.next is None:
+        return node
+    p = reverse_linked_list_recursive(node.next)
+    node.next.next = node
+    node.next = None
+    return p
 
 
 def reverse_linked_list_iterative(node):
+    """
+    Reverses a linked list using iteration
+    Time complexity: O(n)
+    Space complexity: O(1)
+    :param node:
+    :return:
+    """
     prev = None
     curr = node
 
@@ -36,6 +59,29 @@ def reverse_linked_list_iterative(node):
 
 
 if __name__ == '__main__':
+    print("Testing Recursive Reverse Linked List")
+
+    # Create Nodes
+    node_1 = LinkedNode(1)
+    node_2 = LinkedNode(2)
+    node_3 = LinkedNode(3)
+    node_4 = LinkedNode(4)
+
+    # Link Nodes
+    node_1.next = node_2
+    node_2.next = node_3
+    node_3.next = node_4
+
+    # Test initial nodes
+    node_1.print_nodes()
+
+    reverse_linked_list_recursive(node_1)
+
+    node_4.print_nodes()
+    print()
+
+    print("Testing Iterative Reverse Linked List")
+
     # Create Nodes
     node_1 = LinkedNode(1)
     node_2 = LinkedNode(2)
@@ -53,7 +99,5 @@ if __name__ == '__main__':
     reverse_linked_list_iterative(node_1)
 
     node_4.print_nodes()
-
-
 
 

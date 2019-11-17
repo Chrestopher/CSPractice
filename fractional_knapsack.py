e9f04155a5d3af0a92701025a3ce8fa9d1656384
Chres <clc8680@rit.edu>
2019-11-16 20:40:19 -0500

    fractional knapsack tests

--- fractional_knapsack.py
+++ fractional_knapsack.py
@@ -31,7 +31,6 @@ def fractional_knapsack(capacity, items):
                 knapsack[item["name"]] = 1
 
     print("Items in backpack")
-    print()
     for k, v in knapsack.items():
         print(k + ": " + str(v))
 
@@ -40,13 +39,21 @@ def fractional_knapsack(capacity, items):
 
 
 if __name__ == '__main__':
-    # More tests soon
-    # TODO
+    print("Testing with rock, paper, and scissors. Max value should be 2130")
+    print()
     rock = {"name": "Rock", "weight": 50, "value": 100, "ratio": 2}
     paper = {"name": "Paper", "weight": 5, "value": 15, "ratio": 3}
     scissors = {"name": "Scissors", "weight": 30, "value": 150, "ratio": 5}
     bag = [scissors, paper, rock]
-
     max_weight = 430
+    fractional_knapsack(max_weight, bag)
+    print()
 
+    print("Testing with bananas, apples, and strawberries. Max value should be 2417")
+    print()
+    bananas = {"name": "Bananas", "weight": 1, "value": 1, "ratio": 1}
+    apples = {"name": "Apples", "weight": 7, "value": 15, "ratio": 2.14}
+    strawberries = {"name": "Strawberries", "weight": 31, "value": 150, "ratio": 4.84}
+    bag = [bananas, apples, strawberries]
+    max_weight = 505
     fractional_knapsack(max_weight, bag)
